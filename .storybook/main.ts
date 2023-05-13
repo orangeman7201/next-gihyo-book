68086b90a9c75c975602ed97de5ec87deb145bcf
Orangeman7201 <aki.jazz1027@gmail.com>
2023-05-14 02:03:19 +0900

    storybookの設定(途中で断念)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,7 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const path = require("path");
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -6,13 +9,32 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
+  staticDirs: ["public"],
+  babel: async (options) => ({
+    ...options,
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object',
+    ],
+  }),
+  webpackFinal: async (config) => {
+    if (!config.resolve) {
+      config.resolve = {};
+    }
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, "../tsconfig.json"),
+      })
+    ]
+    return config;
+  },
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
   docs: {
     autodocs: "tag",
-  },
-  staticDirs: ["public"]
+  }
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,67 @@
-import type { Preview } from "@storybook/react";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { Preview } from '@storybook/react';
+// import { theme } from "../src/themes";
+// import * as NextImage from "next/image";
+// import React from "react";
 
-const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
+export const parameters = {
+  actions: { argTypeRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
   },
 };
+
+export const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  textarea {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, sans-serif;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  a {
+    cursor: pointer;
+    text-decoration: none;
+    transition: color 0.15s ease;
+    color: #000;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+`;
+
+const preview: Preview = {
+  decorators: [
+    (Story) => (
+      Story()
+    ),
+  ],
+};
+
+// const OriginalNextImage = NextImage.default;
+
+// Object.defineProperty(NextImage, "default", {
+//   configurable: true,
+//   value: (props) =>
+//     typeof props.src === "string" ? (
+//       <OriginalNextImage {...props} unoptimized blurDataURL={props.src} />
+//     ) : (
+//       <OriginalNextImage {...props} unoptimized />
+//     ),
+// });
+
+// Object.defineProperty(NextImage, "__esModule", {
+//   configurable: true,
+//   value: true,
+// });
 
 export default preview;
