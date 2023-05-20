8cbe4c3bc21a01c256307c8141ee4f5d84fbeee5
Orangeman7201 <aki.jazz1027@gmail.com>
2023-05-20 11:59:22 +0900

    flexコンポーネントを実装

---
+++ src/components/layouts/Flex/index.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable */
+import styled from "styled-components";
+import Box, { BoxProps } from "@/components/layouts/Box";
+import type {
+  Responsive,
+  CSSPropertyAlignItems,
+  CSSPropertyAlignContent,
+  CSSPropertyJustifyContent,
+  CSSPropertyJustifyItems,
+  CSSPropertyFlexDirection,
+  CSSPropertyJustifySelf,
+  CSSPropertyFlexWrap,
+  CSSPropertyAlignSelf,
+} from "@/types/styles";
+import { toPropValue } from "@/utils/styles";
+
+type FlexProps = BoxProps & {
+  alignItems?: Responsive<CSSPropertyAlignItems>;
+  alignContent?: Responsive<CSSPropertyAlignContent>;
+  justifyContent?: Responsive<CSSPropertyJustifyContent>;
+  justifyItems?: Responsive<CSSPropertyJustifyItems>;
+  flexWrap?: Responsive<CSSPropertyFlexWrap>;
+  flexDirection?: Responsive<CSSPropertyFlexDirection>;
+  flexGrow?: Responsive<number>;
+  flexShrink?: Responsive<number>;
+  flexBasis?: Responsive<CSSPropertyFlexDirection>;
+  justifySelf?: Responsive<CSSPropertyJustifySelf>;
+  alignSelf?: Responsive<CSSPropertyAlignSelf>;
+  order?: Responsive<number>;
+};
+
+/**
+ * Flexコンポーネント
+ * flexboxの実現に利用する
+ */
+const Flex = styled(Box)<FlexProps>`
+  ${(props) => toPropValue('align-items', props.alignItems, props.theme)}
+  ${(props) => toPropValue('align-content', props.alignContent, props.theme)}
+  ${(props) => toPropValue('justify-content', props.justifyContent, props.theme)}
+  ${(props) => toPropValue('justify-items', props.justifyItems, props.theme)}
+  ${(props) => toPropValue('flex-wrap', props.flexWrap, props.theme)}
+  ${(props) => toPropValue('flex-basis', props.flexBasis, props.theme)}
+  ${(props) => toPropValue('flex-direction', props.flexDirection, props.theme)}
+  ${(props) => toPropValue('flex-grow', props.flexGrow, props.theme)}
+  ${(props) => toPropValue('flex-shrink', props.flexShrink, props.theme)}
+  ${(props) => toPropValue('justify-self', props.justifySelf, props.theme)}
+  ${(props) => toPropValue('align-self', props.alignSelf, props.theme)}
+  ${(props) => toPropValue('order', props.order, props.theme)}
+`
+
+export default Flex
